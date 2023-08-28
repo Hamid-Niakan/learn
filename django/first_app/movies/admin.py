1c541b353db4e7c84e122d02dd4fdf33769ef0a2
Hamid Niakan <hniakan@gmail.com>
2023-08-28 23:32:42 +0330

    create custom admin table

--- django/first_app/movies/admin.py
+++ django/first_app/movies/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Genre, Movie
 
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
 admin.site.register(Genre)
 admin.site.register(Movie)
