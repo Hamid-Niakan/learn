d663aff4e7ecf2ed6bde3bb6efd1907482e5c5ef
Hamid Niakan <hniakan@gmail.com>
2023-08-29 16:45:10 +0330

    pylint django install

--- django/first_app/movies/admin.py
+++ django/first_app/movies/admin.py
@@ -5,6 +5,10 @@ from .models import Genre, Movie
 class GenreAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
 
+class MovieAdmin(admin.ModelAdmin):
+    exclude = ('date_created', )
+    list_display = ('id', 'title', 'number_in_stuck')
+
 
 admin.site.register(Genre, GenreAdmin)
-admin.site.register(Movie)
+admin.site.register(Movie, MovieAdmin)
