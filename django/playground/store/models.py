c06efa7753cc7622d25cb383a4a592148e758faa
Hamid Niakan <hniakan@gmail.com>
2023-09-18 22:56:11 +0330

    adding models to the admin site

--- django/playground/store/models.py
+++ django/playground/store/models.py
@@ -1,3 +1,4 @@
+#pylint: disable = E0307
 from django.db import models
 
 
@@ -11,6 +12,12 @@ class Collection(models.Model):
     featured_product = models.ForeignKey(
         'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -21,6 +28,12 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion, related_name='products')
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
 
 
 class Customer(models.Model):
