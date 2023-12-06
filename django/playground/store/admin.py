6652190b9443541d131b6aed32df17e0f18c28a7
Hamid Niakan <hniakan@gmail.com>
2023-12-06 16:07:00 +0330

    feat: selecting related fields for admin list to show

--- django/playground/store/admin.py
+++ django/playground/store/admin.py
@@ -4,9 +4,13 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
@@ -24,5 +28,14 @@ class CustomerAdmin(admin.ModelAdmin):
     ordering = ['first_name', 'last_name']
     list_per_page = 10
 
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['placed_at', 'payment_status', 'customer_email']
+    ordering = ['placed_at']
+    list_per_page = 10
+    list_select_related = ['customer']
+
+    def customer_email(self, order):
+        return order.customer.email
 
 admin.site.register(models.Collection)
