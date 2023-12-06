40f7febab4a17a67908bdb80b6c6c41ecc2f3492
Hamid Niakan <hniakan@gmail.com>
2023-12-06 15:48:37 +0330

    feat: add computed column for product list

--- django/playground/store/admin.py
+++ django/playground/store/admin.py
@@ -4,9 +4,17 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
     list_per_page = 10
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory == 0:
+            return 'none'
+        elif product.inventory < 10:
+            return 'low'
+        return 'OK'
 
 
 @admin.register(models.Customer)
