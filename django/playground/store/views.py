db009d02db402dc46a37e1de57a35d1b58fce5c2
Hamid Niakan <hniakan@gmail.com>
2023-12-08 01:35:53 +0330

    remove redundant import

--- django/playground/store/views.py
+++ django/playground/store/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import get_object_or_404
-from django.db.models.aggregates import Count
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
