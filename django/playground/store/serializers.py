25ae3e9a0459b5cc2f2c78833c507331ae3d0668
Hamid Niakan <hniakan@gmail.com>
2023-12-08 15:54:33 +0330

    newsted routs

--- django/playground/store/serializers.py
+++ django/playground/store/serializers.py
@@ -1,55 +1,34 @@
 from decimal import Decimal
 from rest_framework import serializers
-from .models import Product, Collection
+from .models import Product, Collection, Review
 
-
-# class CollectionSerializer(serializers.Serializer):
-#     id = serializers.IntegerField()
-#     title = serializers.CharField(max_length=255)
 
 class CollectionSerializer(serializers.ModelSerializer):
+    products_count = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = Collection
         fields = ['id', 'title', 'products_count']
 
-    products_count = serializers.IntegerField(read_only=True)
-
-# class ProductSerializer(serializers.Serializer):
-#     id = serializers.IntegerField()
-#     title = serializers.CharField(max_length=255)
-#     price = serializers.DecimalField(
-#         max_digits=6, decimal_places=2, source='unit_price')
-#     price_with_tax = serializers.SerializerMethodField(
-#         method_name='calculate_tax')
-#     # collection_id = serializers.PrimaryKeyRelatedField(
-#     #     queryset=Collection.objects.all())
-#     # collection_title = serializers.StringRelatedField(source="collection")
-#     collection = CollectionSerializer()
-
-#     def calculate_tax(self, product: Product):
-#         return product.unit_price * Decimal(1.1)
-
 
 class ProductSerializer(serializers.ModelSerializer):
-    # collection = CollectionSerializer()
     price_with_tax = serializers.SerializerMethodField(
         method_name='calculate_tax')
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
 
-    # def create(self, validated_data):
-    #     product = Product(**validated_data)
-    #     # do something here
-    #     product.save()
-    #     return product
-
-    # def update(self, instance, validated_data):
-    #     instance.unit_price = validated_data.get('unit_price')
-    #     instance.save()
-    #     return instance
-
     class Meta:
         model = Product
         fields = ['id', 'title', 'description', 'slug', 'inventory',
                   'unit_price', 'price_with_tax', 'collection']
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['id', 'name', 'description']
+
+    def create(self, validated_data):
+        product_id = self.context['product_id']
+        return Review.objects.create(product_id=product_id, **validated_data)
--- django/playground/store/views.py
+++ django/playground/store/views.py
@@ -2,8 +2,8 @@ from django.db.models.aggregates import Count
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
-from .models import Product, Collection, OrderItem
-from .serializers import ProductSerializer, CollectionSerializer
+from .models import Product, Collection, OrderItem, Review
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
@@ -33,3 +33,13 @@ class CollectionViewSet(ModelViewSet):
                 },
                 status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().destroy(request, *args, **kwargs)
+    
+class ReviewViewSet(ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+
+    def get_serializer_context(self):
+        return { 'product_id': self.kwargs['product_pk']}
