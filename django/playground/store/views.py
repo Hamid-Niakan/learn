77b011e8b4adebf8996cbe37b5013686dadfc69f
Hamid Niakan <hniakan@gmail.com>
2023-12-08 17:40:25 +0330

    pagination

--- django/playground/store/views.py
+++ django/playground/store/views.py
@@ -7,6 +7,7 @@ from rest_framework import status
 from .models import Product, Collection, OrderItem, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 from .filters import ProductFilter
+from .pagination import DefaultPagination
 
 
 # class ProductViewSet(ModelViewSet):
@@ -35,6 +36,7 @@ class ProductViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     # filterset_fields = ['collection_id']
     filterset_class = ProductFilter
+    pagination_class = DefaultPagination
     search_fields = ['title', 'description']
     ordering_fields = ['unit_price', 'last_update']
 
