de09652cb805f5e3e66f4547a60756166448e669
Hamid Niakan <hniakan@gmail.com>
2023-12-08 17:25:00 +0330

    filter.py and search field

--- django/playground/store/views.py
+++ django/playground/store/views.py
@@ -2,9 +2,11 @@ from django.db.models.aggregates import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
 from rest_framework import status
 from .models import Product, Collection, OrderItem, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
+from .filters import ProductFilter
 
 
 # class ProductViewSet(ModelViewSet):
@@ -30,8 +32,10 @@ from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializ
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['collection_id']
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    # filterset_fields = ['collection_id']
+    filterset_class = ProductFilter
+    search_fields = ['title', 'description']
 
     def destroy(self, request, *args, **kwargs):
         if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
