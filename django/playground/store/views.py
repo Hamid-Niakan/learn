48fc680040468191c8058ce8764c60f60b7b7de1
Hamid Niakan <hniakan@gmail.com>
2023-12-08 17:11:41 +0330

    generic filtering

--- django/playground/store/views.py
+++ django/playground/store/views.py
@@ -1,4 +1,5 @@
 from django.db.models.aggregates import Count
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
@@ -6,16 +7,31 @@ from .models import Product, Collection, OrderItem, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
 
+# class ProductViewSet(ModelViewSet):
+#     serializer_class = ProductSerializer
+
+#     def get_queryset(self):
+#         queryset = Product.objects.all()
+#         collection_id = self.request.query_params.get('collection_id')
+#         if collection_id is not None:
+#             queryset = queryset.filter(collection_id=collection_id)
+
+#         return queryset
+
+#     def destroy(self, request, *args, **kwargs):
+#         if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
+#             return Response(
+#                 {
+#                     'error': 'there is an order item with this product'
+#                 },
+#                 status=status.HTTP_405_METHOD_NOT_ALLOWED)
+#         return super().destroy(request, *args, **kwargs)
+
 class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get('collection_id')
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
-
-        return queryset
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['collection_id']
 
     def destroy(self, request, *args, **kwargs):
         if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
